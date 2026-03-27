ce891433866bd48ce83c0d8ff118903a1adc1646
ddinislam <251118@astanait.edu.kz>
2026-03-27 09:17:52 +0500

    second commit

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-print( "==============================")
-print("      STUDENT REPORT CARD       ")
-print( "==============================")
+print("==============================")
+print("      STUDENT REPORT CARD     ")
+print("==============================")
 
-name = input ("Student: ")
+name = input("Student: ")
+
 x = float(input("Math: "))
 if x < 0 or x > 100:
-    print("error the grade will be at 0 to 100! ")
+    print("error the grade will be at 0 to 100!")
     exit()
 
-y = float(input("Physics: "))
+y = float(input("Python: "))
 if y < 0 or y > 100:
-    print("error the grade will be at 0 to 100! ")
+    print("error the grade will be at 0 to 100!")
     exit()
 
-z = float(input("Python: "))
+z = float(input("Physics: "))
 if z < 0 or z > 100:
-    print("error the grade will be at 0 to 100! ")
+    print("error the grade will be at 0 to 100!")
     exit()
 
-print ("------------------------------")
-avg = (x + y + z) /3
-avg = round(avg,2)
-print(avg)
-gpa = round(avg / 25, 2)
-print (gpa)
-if avg > 90:
-    print("Scholarship: 35000KZT")
+
+
+print("------------------------------")
+
+avg = (x + y + z ) / 3
+avg = round(avg, 2)
+print("Average:", avg)
+
+if avg >= 90:
+    grade = "A"
+elif avg >= 75:
+    grade = "B"
+elif avg >= 60:
+    grade = "C"
+elif avg >= 50:
+    grade = "D"
 else:
-    print("you dont have scholarship( ")
-print("Scholarship:", avg >= 90)
-print("Perfect score:", avg == 100)
+    grade = "F"
+
+print("Letter grade: ", grade)
+gpa = round(avg / 25, 2)
+print("GPA:", gpa)
+
+if (x > 70 and y > 70 and z > 70 and avg >= 90):
+    print("Scholarship: 52000")
+else:
+    print("You don't have scholarship")
+
+
+
 print("==============================")
