63f48d6a0580064b43ebaac0419145f84095b1c0
ddinislam <251118@astanait.edu.kz>
2026-03-27 09:49:57 +0500

    second

--- main.py
+++ main.py
@@ -9,21 +9,19 @@ if x < 0 or x > 100:
     print("error the grade will be at 0 to 100!")
     exit()
 
-y = float(input("Python: "))
+y = float(input("Physics: "))
 if y < 0 or y > 100:
     print("error the grade will be at 0 to 100!")
     exit()
 
-z = float(input("Physics: "))
+z = float(input("Python: "))
 if z < 0 or z > 100:
     print("error the grade will be at 0 to 100!")
     exit()
 
-
-
 print("------------------------------")
 
-avg = (x + y + z ) / 3
+avg = (x + y + z) / 3
 avg = round(avg, 2)
 print("Average:", avg)
 
@@ -38,15 +36,27 @@ elif avg >= 50:
 else:
     grade = "F"
 
-print("Letter grade: ", grade)
-gpa = round(avg / 25, 2)
-print("GPA:", gpa)
+print("Letter grade:", grade)
 
-if (x > 70 and y > 70 and z > 70 and avg >= 90):
-    print("Scholarship: 52000")
-else:
-    print("You don't have scholarship")
+scholarship = (x >= 70 and y >= 70 and z >= 70 and avg >= 90)
+print("Scholarship:", scholarship)
 
+grades = [x, y, z]
+subjects = ["Math", "Physics", "Python"]
 
+for i in range(len(grades)):
+    if grades[i] >= 90:
+        print(subjects[i] + ":", "Excellent")
+    elif grades[i] >= 75:
+        print(subjects[i] + ":", "Good")
+    elif grades[i] >= 60:
+        print(subjects[i] + ":", "Satisfactory")
+    else:
+        print(subjects[i] + ":", "Fail")
 
+print("------------------------------")
+print("Name uppercase:", name.upper())
+print("Name lowercase:", name.lower())
+print("Name length:", len(name))
+print("Masked name:", name.replace(name[0], "*", 1))
 print("==============================")
